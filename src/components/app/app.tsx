619379c3e77c3a6e068f2f0f06a4f640f8c57202
Franz <fernandofranquiz@gmail.com>
2023-10-30 21:28:07 +0100

    Add info and gentlement componet

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,1 +1,16 @@
+import { Gentleman } from '../cards/gentleman';
+import { Info } from '../info/info';
+import '../styles.scss/style.css';
+import { gentleman } from '../../data/repo';
 
+export function App() {
+  const title = 'The pointing gentlemen';
+  const subtitle = 'gentlemen pointing at you';
+
+  return (
+    <div>
+      <Info title={title} subtitle={subtitle}></Info>
+      <Gentleman gentleman={gentleman}></Gentleman>
+    </div>
+  );
+}
